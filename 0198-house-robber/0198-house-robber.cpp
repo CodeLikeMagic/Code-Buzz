c9080d64dcae403c8efced92cf188856d6efe605
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2022-10-21 19:58:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (49.63%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.size() == 0){
+            return 0;
+        }
+        
+        int N = nums.size();
+        int dp[N+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i=1; i<N; i++){
+            
+            ///if want values can be skipped 
+            ///[1,2,3,1,1,100] - 5 / 103/ 104
+            ///since adjancent values can't be taken
+            ///choose bw current max and if prev max and curr index make max
+            dp[i+1] = max(dp[i], dp[i-1]+nums[i]);
+        }
+        
+        return dp[N];
+    }
+};
