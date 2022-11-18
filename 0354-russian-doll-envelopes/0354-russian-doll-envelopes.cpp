11e1d9f593f1f3d70d7d7a2c2b9788b2f74ad6aa
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2022-11-18 22:17:33 +0530

    Time: 805 ms (54.49%), Space: 77.6 MB (68.65%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool static comp( vector<int>&a, vector<int>&b){
+        if(a[0]==b[0])
+        {
+            return a[1]>b[1];
+        }
+        else{
+            return a[0]<b[0];
+        }
+   }
+    int maxEnvelopes(vector<vector<int>>& envelopes) 
+    {
+        
+        int n=envelopes.size();
+        sort(envelopes.begin(),envelopes.end(),comp);
+        vector<int>ans;
+        ans.push_back(envelopes[0][1]);
+        for(int i=1;i<n;i++)
+        {
+            if(envelopes[i][1]>ans.back()){
+                ans.push_back(envelopes[i][1]);
+            }
+            else
+            {
+                int idx=lower_bound(ans.begin(),ans.end(),envelopes[i][1]) - ans.begin();
+                ans[idx]=envelopes[i][1];
+            }
+        }
+        
+        return ans.size();
+    }
+};
