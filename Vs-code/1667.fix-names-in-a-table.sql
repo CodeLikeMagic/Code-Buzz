0d72b1e73366c3a5d7b72885d416a833507b1ea2
01DEEKSHA <deekshapatidar9@gmail.com>
2022-12-06 12:45:12 +0530

    Create 1667.fix-names-in-a-table.sql

---
+++ Vs-code/1667.fix-names-in-a-table.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode id=1667 lang=mysql
+--
+-- [1667] Fix Names in a Table
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+-- @lc code=end
+select user_id , 
+concat(upper(substr(name,1,1)),lower(substr(name,2,50))
+as name
+from users
+ordere by user_id
