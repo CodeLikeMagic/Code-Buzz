19b0c315e659b7b67dee9daaefea67d012a22cba
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2023-04-04 23:37:08 +0530

    Time: 29 ms (23.39%), Space: 23.1 MB (80.42%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        char temp;
+        for(int i=0, j=s.size()-1; i<(s.size()/2); i++,j--){
+            temp = s[j];          
+            s[j] = s[i];
+            s[i] = temp;
+        }
+    }
+};
