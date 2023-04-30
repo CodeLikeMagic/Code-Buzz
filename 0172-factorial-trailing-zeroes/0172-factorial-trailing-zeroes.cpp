0457a725155435996fcb9257664c690215eead9e
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2023-04-30 19:04:34 +0530

    Time: 5 ms (32.82%), Space: 6 MB (65.67%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int result = 0;
-        for(long long i=5; n/i>0; i*=5){
+        for(long long i=5; i<=n; i*=5){
             result += (n/i);
         }
         return result;
