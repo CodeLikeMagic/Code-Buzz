7c563c36e9e1be8c8ad2097d2a7e9121abf27207
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2024-04-07 14:31:57 +0530

    Time: 9 ms (5.91%), Space: 11.6 MB (6.14%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    void func(int index, vector<int>& nums, vector<vector<int>> &answer)
-    {
-        if(index == nums.size())
-        {
-            answer.push_back(nums);
-            return;
+    void solve(vector<int>& nums, int start, vector<vector<int>> &result){
+        
+        if(start == nums.size()){
+            result.push_back(nums);
         }
-        for(int i=index; i<nums.size(); i++)
-        {
-             swap(nums[index], nums[i]);    //swap
-             func(index+1,nums,answer);
-             swap(nums[i], nums[index]);    //reswap backtrack
+        
+        unordered_set<char> mp;
+        
+        for(int i=start; i<nums.size(); i++){
+            
+            if(mp.find(nums[i]) == mp.end()){
+                mp.insert(nums[i]); //keep distinct 
+                
+                swap(nums[start], nums[i]); //make changes
+                
+                solve(nums, start+1, result);   //rc
+                
+                swap(nums[start], nums[i]); //backtrack
+            }           
         }
-    }   
+    }
     
-public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> answer;
-        func(0, nums, answer);
-        return answer;
+       
+        vector<vector<int>> result;
+        
+        solve(nums, 0, result);
+        
+        return result;
     }
 };
