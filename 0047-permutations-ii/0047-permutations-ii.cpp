354816fbad67cff3705a60ca732b78dbd0b68538
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2024-04-07 14:35:12 +0530

    Time: 9 ms (49.73%), Space: 14.3 MB (19.45%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+     void solve(vector<int>& nums, int start, vector<vector<int>> &result){
+        
+        if(start == nums.size()){
+            result.push_back(nums);
+        }
+        
+        unordered_set<char> mp;
+        
+        for(int i=start; i<nums.size(); i++){
+            
+            if(mp.find(nums[i]) == mp.end()){
+                mp.insert(nums[i]); //keep distinct 
+                
+                swap(nums[start], nums[i]); //make changes
+                
+                solve(nums, start+1, result);   //rc
+                
+                swap(nums[start], nums[i]); //backtrack
+            }           
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        vector<vector<int>> result;
+        
+        solve(nums, 0, result);
+        
+        return result;
+    }
+};
