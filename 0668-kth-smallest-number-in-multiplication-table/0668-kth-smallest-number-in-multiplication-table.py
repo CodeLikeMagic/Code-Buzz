6a4984e8523402b93e9b25bfaa186805a84f4871
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2024-12-16 18:53:44 +0530

    Time: 574 ms (10.47%), Space: 17.3 MB (13.99%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -4,14 +4,17 @@ class Solution:
             count = 0
             for val in range(1, m+1): #count row by row
                 add = min(num // val, n)
+                #print("num : ",num," val : ",val)
                 if add == 0: #early exit
                     break
                 count += add
+            #print("count : ",count)
             return count >= k
         left = 1
         right = n * m
         while left < right:
             mid = left + (right - left) // 2
+            #print(mid)
             if enough(mid):
                 right = mid
             else:
