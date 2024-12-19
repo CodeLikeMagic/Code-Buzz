8b6fdf78056b8f75cc3fb2595347e6a0e8e442cf
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2024-12-19 13:34:53 +0530

    Time: 3 ms (21.54%), Space: 17.8 MB (8.85%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.py
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -1,17 +1,20 @@
 class Solution:
     def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
-        def enough(num):
+        def enough(mid):
             ab = a * b // math.gcd(a, b)
             ac = a * c // math.gcd(a, c)
             bc = b * c // math.gcd(b, c)
             abc = a * bc // math.gcd(a, bc)
+            print("ab, ac, bc, abc", ab, ac, bc, abc)
             total = mid//a + mid//b + mid//c - mid//ab - mid//ac - mid//bc + mid//abc
+            print("total ", total)
             return total >= n
         
         left = 1
         right = 10**10
         while left < right:
             mid = left + (right - left) // 2
+            print("mid ", mid)
             if enough(mid):
                 right = mid
             else:
