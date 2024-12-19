25544411ce6f69e6fcb55f8e37acc83d60834108
CodeLikeMagic <103945418+CodeLikeMagic@users.noreply.github.com>
2024-12-19 13:45:47 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (8.85%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.py
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -5,16 +5,16 @@ class Solution:
             ac = a * c // math.gcd(a, c)
             bc = b * c // math.gcd(b, c)
             abc = a * bc // math.gcd(a, bc)
-            print("ab, ac, bc, abc", ab, ac, bc, abc)
+            #print("ab, ac, bc, abc", ab, ac, bc, abc)
             total = mid//a + mid//b + mid//c - mid//ab - mid//ac - mid//bc + mid//abc
-            print("total ", total)
+            #print("total ", total)
             return total >= n
         
         left = 1
         right = 10**10
         while left < right:
             mid = left + (right - left) // 2
-            print("mid ", mid)
+            #print("mid ", mid)
             if enough(mid):
                 right = mid
             else:
